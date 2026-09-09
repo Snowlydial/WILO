51e0c1befb4c6393a73163b54f3f6e1998d91be6
Ray <117043408+Snowlydial@users.noreply.github.com>
2026-09-09 13:52:41 +0300

    Initial commit

---
+++ backend/src/main/java/com/snowlydial/wilo/WiloApplication.java
@@ -0,0 +1,13 @@
+package com.snowlydial.wilo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiloApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/snowlydial/wilo/WiloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snowlydial.wilo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
