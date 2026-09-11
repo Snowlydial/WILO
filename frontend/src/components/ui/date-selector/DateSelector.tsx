2ab2e0c071b82cb9b546074dc0f6309315229c32
Ray <117043408+Snowlydial@users.noreply.github.com>
2026-09-11 11:10:31 +0300

    2.12e-Added Prev Next Week

--- frontend/src/components/ui/date-selector/DateSelector.tsx
+++ frontend/src/components/ui/date-selector/DateSelector.tsx
@@ -20,24 +20,37 @@ function getWeekDates(selectedDate: Date): Date[] {
 export default function DateSelector() {
     const now = new Date;
     const [selectedDate, setSelectedDate] = useState(now);
+    const [viewDate, setViewDate] = useState(now);
 
-    const selectedMonth = selectedDate.getMonth();
-    const selectedYear = selectedDate.getFullYear();
+    const selectedMonth = viewDate.getMonth();
+    const selectedYear = viewDate.getFullYear();
 
     function handleMonthChange(month: number) {
         const updated = new Date(selectedDate);
         updated.setMonth(month);
-        setSelectedDate(updated);
+        setViewDate(updated);
     }
 
     function handleYearChange(year: number) {
         const updated = new Date(selectedDate);
         updated.setFullYear(year);
-        setSelectedDate(updated);
+        setViewDate(updated);
     }
 
-    const weekDates = getWeekDates(selectedDate);
+    const weekDates = getWeekDates(viewDate);
     const dayTag: string[] = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+
+    function handlePrevWeek() {
+        const updated = new Date(viewDate);
+        updated.setDate(viewDate.getDate() - 7);
+        setViewDate(updated);
+    }
+
+    function handleNextWeek() {
+        const updated = new Date(viewDate);
+        updated.setDate(viewDate.getDate() + 7);
+        setViewDate(updated);
+    }
     
     return (
         <div className="date-selector">
@@ -50,7 +63,13 @@ export default function DateSelector() {
                 onYearChange={handleYearChange}
             />
             <div className="week-row">
-                {weekDates.map((date) => (
+                <button
+                    className='selector-arrow'
+                    onClick={handlePrevWeek}
+                >
+                    <img src="/icons/arrow-left.svg" alt="previous"/>
+                </button>
+                {weekDates.map((date, index) => (
                     <DayCard
                         key={date.toISOString()}
                         day={String(date.getDate())}
@@ -59,6 +78,12 @@ export default function DateSelector() {
                         onDayChange={() => setSelectedDate(date)}
                     />
                 ))}
+                <button
+                    className='selector-arrow'
+                    onClick={handleNextWeek}
+                >
+                    <img src="/icons/arrow-right.svg" alt="next"/>
+                </button>
             </div>
         </div>
     )
